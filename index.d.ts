323b9f512f7dc890310fff84c9a90030bf6aea02
Ammar <armmarov@gmail.com>
2024-11-10 16:51:18 +0800

    Fix index

--- index.d.ts
+++ index.d.ts
@@ -1,29 +1,31 @@
-export class KeyPair {
-  getEncPublicKey(encPrivateKey: string): string
+declare module 'keypair' {
+  export function getEncPublicKey(encPrivateKey: string): string
 
-  getAddress(encPublicKey: string): string
+  export function getAddress(encPublicKey: string): string
 
-  parsePrivateKey(encPrivateKey: string): string
+  export function parsePrivateKey(encPrivateKey: string): string
 
-  parsePublicKey(encPublicKey: string): string
+  export function parsePublicKey(encPublicKey: string): string
 
-  getKeyPair(): any
+  export function getKeyPair(): any
 
-  checkEncPrivateKey(encPrivateKey: string): boolean
+  export function checkEncPrivateKey(encPrivateKey: string): boolean
 
-  checkEncPublicKey(encPublicKey: string): boolean
+  export function checkEncPublicKey(encPublicKey: string): boolean
 
-  checkAddress(address: string): boolean
+  export function checkAddress(address: string): boolean
 }
 
-export class keystore {
-  encrypt(encPrivateKey: string, password: string, callback: any): void
+declare module 'keystore' {
+  export function encrypt(encPrivateKey: string, password: string, callback: any): void
 
-  decrypt(keystore: any, password: string, callback: any): string
+  export function decrypt(keystore: any, password: string, callback: any): string
 }
 
-export class signature {
-  sign(message: string, encPrivateKey: string): string
+declare module 'signature' {
+  export function sign(message: string, encPrivateKey: string): string
 
-  verify(message: any, signature: string, encPublicKey: any): boolean
+  export function verify(message: any, signature: string, encPublicKey: any): boolean
 }
+
+export { keypair, signature, keystore }
